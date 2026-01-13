e757809542b414fbd7396995f8c7b3168418aae8
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-01-12 20:04:29 -0500

    Se agrega recuperar entero

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 function saludar(){
     let nombre = recuperarTexto("txtNombre");
-    console.log(nombre);
+    let apellido = recuperarTexto("txtApellido");
+    console.log(nombre+" "+apellido);
+}
+
+function sumar(){
+   let numero1=recuperarEntero("txtNombre");
 }
--- utils.js
+++ utils.js
@@ -3,4 +3,10 @@ function recuperarTexto(idCaja){
     let txtCaja = document.getElementById(idCaja);
     let valorCaja = txtCaja.value;
     return valorCaja;
-};
+}
+
+function recuperarEntero(idCaja){
+    let valorTexto=recuperarTexto(idCaja);
+    let valorEntero=parseInt(valorTexto);
+    return valorEntero;
+}
